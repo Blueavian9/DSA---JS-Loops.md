41a18af7de15e4307b26561e157744cd1d21f239
Cesar Aguilar <Blueavian9@gmail.com>
2024-10-24 11:24:36 -0700

    main

--- doWhileLoop.js
+++ doWhileLoop.js
@@ -1,24 +1,52 @@
-/* 1. DO WHILE LOOP
+/* 1a. DO WHILE LOOP:
 ENSURE A USER ENTERS 
-A NUMBER GREATER THAN 10: */
-let userInput;
+A NUMBER GREATER THAN 10 */
 
-do {
-    userInput = prompt("Enter a number greater than 10:");
-} while (userInput <= 10); 
+// let userInput;
+// do {
+//     userInput = prompt("Enter a number greater than 10:");
+// } while (userInput <= 10); 
 
-/* 2. DISPLAY NUMBERS
-FROM 1 - 5 AT LEAST ONCE: */ 
-let i = 1; 
-do { 
-    console.log(i); 
-    i++;
-} while (i <= 5); 
+/* 1b. DO WHILE LOOP OPPOSITE:
+CONDITION FOR STOPPING (OPPOSITE OF CONTINUING)
+example: STOPS WHEN A CERTAIN CONDITION IS MET,
+EVEN THOUGH THE LOOP RUNS ONCE:
+*/ 
+
+// let i = 10;
+// do {
+//     console.log(i); 
+//     i--;
+// } while (i > 5);
+
+
+
+
+/* 2a. DISPLAY NUMBERS
+FROM 1 - 5 COUNTING UP AT LEAST ONCE: */ 
+
+// let i = 1; 
+// do { 
+//     console.log(i); 
+//     i++;
+// } while (i <= 5); 
+
+
+/* 2b. (OPPOSITE) COUNTING DOWN: */
+
+// let i = 5;
+// do {
+//     console.log(i);
+//     i--;
+// } while (i > 0); 
+
+
+
 
 /* 3. RUN A LOOP THAT CONTINUES
 UNTIL A USER TYPES: "EXIT" */ 
-let command;
-do {
-    command = prompt("Enter a command or 'exit' to quit:");
-    console.log("You entered:", command);
-} while (command !== "exit");
+// let command;
+// do {
+//     command = prompt("Enter a command or 'exit' to quit:");
+//     console.log("You entered:", command);
+// } while (command !== "exit");
