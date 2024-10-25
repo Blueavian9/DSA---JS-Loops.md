f94c0afb5380fda459182c180197a66fe051b6bd
Cesar Aguilar <Blueavian9@gmail.com>
2024-10-25 09:52:19 -0700

    updated commit

--- DSA-Interview-Prep.js/SlidingWindow.js
+++ DSA-Interview-Prep.js/SlidingWindow.js
@@ -14,3 +14,26 @@ function maxSumSubarray(arr, k) {
     
     return maxSum;
 }
+
+// example 2: 
+// Longest Substring with K Distinct Characters
+
+function longestDistinct(s, k) {
+    let start = 0, maxLength = 0; 
+    const charMap = new Map(); 
+
+    for (let end = 0; end < s.length; end++) {
+        charMap.set(s[end], (charMap.get(s[end]) || 0) + 1); 
+        while (charMap.size > k) {
+            charMap.set(s[start], charMap.get(s[start]) - 1); 
+            if (charMap.get(s[start]) === 0) charMap.delete(s[start]);
+            start++;
+    }
+    maxLength = Math.max(maxLength, end - start + 1);
+}
+
+return maxLength; 
+}
+
+
+// example 3: Minimum Size Subarray Sum 
